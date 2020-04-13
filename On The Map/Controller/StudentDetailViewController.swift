65a326acb2d110847b43da6ae25ba57b6bc277d4
Michael <mhaviv18@gmail.com>
2020-04-12 23:38:29 -0400

    Added Student Detail View Controller file

---
+++ On The Map/Controller/StudentDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudentDetailViewController.swift
+//  On The Map
+//
+//  Created by Michael Haviv on 2/18/20.
+//  Copyright © 2020 Michael Haviv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudentDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
