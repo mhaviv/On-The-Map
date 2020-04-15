e79645304f9879eec6ddc044bef5a88d05593ef2
Michael <mhaviv18@gmail.com>
2020-04-15 14:00:23 -0400

    Created constants for user defaults

---
+++ On The Map/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  On The Map
+//
+//  Created by Michael Haviv on 4/15/20.
+//  Copyright © 2020 Michael Haviv. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    enum UserDefaults {
+        static let userSession = "UserSession"
+        
+    }
+}
